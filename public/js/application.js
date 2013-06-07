738601192545930ea5826cb71be34545f031d140
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-07 14:51:56 -0500

    jquery methods

--- public/js/application.js
+++ public/js/application.js
@@ -1,24 +1,21 @@
 $(document).ready(function() {
   $('#back').hide();
+  $('.feedback').hide();
+
   $('body').on('click', '#input', function() { //click on a button, delegating a answer?
     $(this).prev().children('#back').show('hidden'); //the hidden something
+    $(this).next().show('.feedback'); 
+    // $('#input').hide(); //the hidden something
   });
+
 
   $('.feedback input').click(function() {
     $.post('/card', function(card) {
       $('#card').children().remove();
       $('#card').append(card);
       $('#back').hide();
+      $('.feedback').hide();
     })
   });_
-
-  
-
 });
 
-// when a click happens on .feedback input
-// $.post(... , function(data) { 
-//  use data... 
-//  replaces text of $(#front).text( replacement text )
-//  replaces back
-// })
