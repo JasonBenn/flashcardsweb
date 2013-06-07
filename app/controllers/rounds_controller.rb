eb27a70b2971a8e0553b0f7afeb7662fbf3bfa6d
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-07 17:33:22 -0500

    Got history to display

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,8 +1,11 @@
-get '/rounds' do
-  #erb :
+get '/history' do
+  @user = User.find(current_user)
+  @rounds = @user.rounds
+  erb :rounds
 end
 
 post '/increment_counter' do
   puts params[:pressed].inspect 
   
 end
+
