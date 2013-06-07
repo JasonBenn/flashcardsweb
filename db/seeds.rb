f2c2ed9644e26048061e53e9530b0cdded71c487
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-07 18:01:14 -0500

    History page

--- db/seeds.rb
+++ db/seeds.rb
@@ -259,3 +259,36 @@ end
     :deck_id => (1..10).to_a.sample) 
 end
 
+__END__
+
+"The Chronic, 2001, Detox": "Dr. Dre"
+"I Am...The Autobiography, God's Son, Hip Hop Is Dead": "Nas"
+"Elephunk, Monkey Business, The E.N.D.": "Black Eyed Peas"
+"All Hail the Queen, Black Reign, Trav'lin' Light": "Queen Latifah"
+"Radio, Bigger and Deffer, G.O.A.T.": "LL Cool J"
+"Rock the House, He's the DJ, I'm the Rapper, Homebase": "DJ Jazzy Jeff & The Fresh Prince"
+"Ready To Die, Life After Death, Born Again": "The Notorious B.I.G."
+"Word Of Mouf, Chicken-n-Beer, Release Therapy": "Ludacris"
+"Yo! Bum Rush the Show, It Takes a Nation of Millions to Hold Us Back, Fear of a Black Planet": "Public Enemy"
+"Jealous Ones Still Envy (J.O.S.E.), Loyalty, Me, Myself & I": "Fat Joe"
+"Blunted on Reality, The Score, Bootleg Versions": "The Fugees"
+"Ill Na Na, Chyna Doll, Broken Silence": "Foxy Brown"
+"Doggystyle, Tha Doggfather, R&G (Rhythm & Gangsta): The Masterpiece": "Snoop Dogg"
+"Rhyme Pays, O.G. Original Gangster, Home Invasion": "Ice-T"
+"Country Grammar, Nellyville, Suit": "Nelly"
+"Albums": "Rapper"
+"E 1999 Eternal, The Art of War, Strength & Loyalty": "Bone Thugs-N-Harmony"
+"Hot, Cool & Vicious, Blacks' Magic, Very Necessary": "Salt-N-Pepa"
+"Get Rich Or Die Tryin', The Massacre, Curtis": "50 Cent"
+"Hard Core, La Bella Mafia, The Naked Truth": "Lil' Kim"
+One Day It'll All Make Sense, Be, Finding Forever Common
+"Straight Outta Compton, 100 Miles and Runnin', Niggaz4life": "N.W.A."
+"Vol. 2... Hard Knock Life, The Black Album, Kingdom Come": "Jay-Z"
+"Aquemini, Stankonia, Speakerboxxx / The Love Below": "Outkast"
+Strictly 4 My N.I.G.G.A.Z, Me Against The World, All Eyez On Me 2Pac
+It's Dark and Hell Is Hot, Grand Champ, Year of the Dog...Again DMX
+"King Of Rock, Raising Hell, Tougher Than Leather": "Run D.M.C."
+Paid in Full, Follow the Leader, Let the Rhythm Hit 'Em Eric B. & Rakim
+Supa Dupa Fly, Miss E… So Addictive, Under Construction Missy Elliott
+"The Coming, When Disaster Strikes, Genesis": "Busta Rhymes"
+"Ruff Ryders' First Lady, Scorpion, Eve-Olution": "Eve"
